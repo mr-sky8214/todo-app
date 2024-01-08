0a0d5c44caa00c0a3146ef279d596aa5f68a736a
Akash Kamble <kambleakash8214@gmail.com>
2024-01-08 23:40:12 +0530

    using props and hooks

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './Counter.css'
+import CounterButton from './CounterButton';
+
+export default  function Counter() {
+    
+    // [0, ƒ]
+    const [count, setCount] = useState(0)
+
+    function incrementCounterParentFunction(by) {
+        setCount(count + by);
+    }
+
+    function decrementCounterParentFunction(by) {
+        setCount(count - by);
+    }
+
+    function resetCounter () {
+        setCount(0)
+    }
+
+    return (
+        <div>    
+            
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton  by={2} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton  by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <button className="resetButton" onClick={resetCounter} >Reset</button>
+        </div>
+    );
+}
